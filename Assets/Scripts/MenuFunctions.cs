66bba303164e6208978dc293ee67ad9a175abc82
Luisina Ilardo <louseen.smith@gmail.com>
2024-07-31 14:12:23 +0200

    MiglioramentiUI

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -1,19 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using Unity.UI;
 
 public class MenuFunctions : MonoBehaviour
 {
+    public AudioSource clickSound;
+    private bool clicked;
     public void LoadScene(int index)
     {
-        if(index > 0)
+        if (index > 0)
         {
             Time.timeScale = 1;
         }
-        SceneManager.LoadScene(index);
+        if(!clicked) 
+        {
+            clicked = true;
+            StartCoroutine(DelayLoadScene(index));
+        }
     }
 
     public void Quit()
     {
+        StartCoroutine(DelayQuit());
+    }
+
+    IEnumerator DelayLoadScene(int index) // Il tasto deve richiamare DelayLoadScene (non più LoadScene) per fare in modo che si senta il sound effect del tasto.
+    {
+        clickSound.Play();
+        yield return new WaitForSeconds(0.25f);
+        SceneManager.LoadScene(index);
+
+    }
+
+
+    IEnumerator DelayQuit()
+    {
+        clickSound.Play();
+        yield return new WaitForSeconds(0.25f);
         Application.Quit();
     }
+
+    
 }
